bcb7bdc8dec7c97a636120c9edad0e9d008120db
unknown <SSDD@POAPCA01LC0417.duoc.acad>
2026-06-15 17:22:44 -0400

    eureka y gateway

---
+++ gateway/src/test/java/com/fullstack/SoftwareAviones/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack.SoftwareAviones.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
